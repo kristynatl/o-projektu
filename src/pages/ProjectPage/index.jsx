0b077a3aee527b3bc6a4ca35ddfb8ba9d7cc67d2
Kristýna Tlapáková <tlapakova24k@gmail.com>
2023-11-12 20:43:52 +0100

    Dokončení ProjectPage

--- src/pages/ProjectPage/index.jsx
+++ src/pages/ProjectPage/index.jsx
@@ -5,12 +5,30 @@ export const ProjectPage = () => {
   return (
     <div className="main">
       <HomeIcon />
-      <h1>Jak ti to jen říct?</h1>
-      <p className="description">
-        Co je to za projekt, kdy a proč vzniknul, proč je důležité učit děti
-        konsentu, kdy a jak začít, proč to může být pro rodiče složité a proč je
-        ten projekt dobrý.
-      </p>
+      <img
+        className="image-ai"
+        src="./img/teaching-consent.png"
+        alt="Rodič vysvětlující dítěti konsent podle Midjourney"
+      />
+      <div className="introduction">
+        <h1>Projekt "Jak ti to jen říct?"</h1>
+        <p>
+          Rodiče a další pečující osoby si dnes již uvědomují, jak je důležité
+          osvětlit dětem přiměřeně jejich věku témata související s konsentem a
+          intimitou, nicméně často nevědí, jak na to. Bohužel, zdrojů a
+          praktických nástrojů, které by jim to usnadnily, je málo.
+        </p>
+        <p>
+          "Jak ti to jen říct?" je interaktivní webová aplikace, která dospělým
+          umožní vyzkoušet si nanečisto, jak by takové rozhovory v každodenních
+          situacích s dítětem mohly probíhat.
+        </p>
+        <p>
+          Obrázek z produkce umělé inteligence Midjourney vygeneroval "children
+          book illustration of a nervous parent talking to a child about
+          consent".
+        </p>
+      </div>
       <Buttons value1="Chci vědět víc" value2="Čí to byl nápad" />
     </div>
   );
